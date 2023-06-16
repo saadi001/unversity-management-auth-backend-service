e395d05dfca7dd49721bc65a16cd10d2b4a53512
saadi001 <sheikhsadi1620@gmail.com>
2023-06-16 15:33:39 +0600

    mapper added to academic semester

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,9 +1,15 @@
+import ApiError from '../../../errors/ApiErrors';
+import { academicSemesterTitleCodeMapper } from './academicSemester.constant';
 import { IAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.model';
+import httpStatus from 'http-status';
 
 const createAcademicSemester = async (
   payload: IAcademicSemester
 ): Promise<IAcademicSemester> => {
+  if (academicSemesterTitleCodeMapper[payload.title] !== payload.code) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid semester code.');
+  }
   const result = await AcademicSemester.create(payload);
   return result;
 };
