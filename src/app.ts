274db4ce76f0e69a2f2ef48e3cf6be4531f81727
saadi001 <sheikhsadi1620@gmail.com>
2023-07-13 01:36:32 +0600

    searching filter and pagination done and route for single semester done

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import express, { Application } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import globalErrorHandler from './middlewars/globalErrorHandlers';
 import routes from './app/routes';
+import httpStatus from 'http-status';
 const app: Application = express();
 
 app.use(cors());
@@ -15,5 +16,18 @@ app.use('/api/v1', routes);
 
 // global error handler
 app.use(globalErrorHandler);
+
+// handle not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found!',
+    errorMessage: {
+      path: req.originalUrl,
+      message: 'Api not found.',
+    },
+  });
+  next();
+});
 
 export default app;
